a134dc4f002a81882d6b9dfc7a2ac3525b9494a7
Mike <33880049+Hiercy@users.noreply.github.com>
2018-12-05 16:32:36 +0300

    Context menu

--- ScrollingText/app/src/main/java/com/example/volk1/scrollingtext/MainActivity.java
+++ ScrollingText/app/src/main/java/com/example/volk1/scrollingtext/MainActivity.java
@@ -1,13 +1,57 @@
 package com.example.volk1.scrollingtext;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.ContextMenu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView mArticle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mArticle = findViewById(R.id.article);
+
+        registerForContextMenu(mArticle);
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu_main, menu);
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
+
+        switch (item.getItemId()) {
+            case R.id.edit:
+                displayToast(getString(R.string.edit));
+                return true;
+            case R.id.share:
+                displayToast(getString(R.string.share));
+                return true;
+            case R.id.delete:
+                displayToast(getString(R.string.delete));
+                return true;
+            default:
+                return super.onContextItemSelected(item);
+        }
+    }
+
+    private void displayToast(String message) {
+        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
     }
 }
