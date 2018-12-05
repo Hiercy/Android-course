db885cc04392975bd28e14e89437308e25c2984f
Mike <33880049+Hiercy@users.noreply.github.com>
2018-12-05 23:51:36 +0300

    Date and Alert

---
+++ DialogForAlertAndDatePicker/app/src/main/java/com/example/volk1/dialogforalertanddatepicker/DatePickerFragment.java
@@ -0,0 +1,35 @@
+package com.example.volk1.dialogforalertanddatepicker;
+
+
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.DialogFragment;
+import android.support.v4.app.Fragment;
+import android.widget.DatePicker;
+
+import java.util.Calendar;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class DatePickerFragment extends DialogFragment implements DatePickerDialog.OnDateSetListener {
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        final Calendar calendar = Calendar.getInstance();
+        int year = calendar.get(Calendar.YEAR);
+        int month = calendar.get(Calendar.MONTH);
+        int day = calendar.get(Calendar.DAY_OF_MONTH);
+
+        return new DatePickerDialog(getActivity(), this, year, month, day);
+    }
+
+    @Override
+    public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+        MainActivity activity = (MainActivity) getActivity();
+        activity.proccessDatePickerResult(year, month, day);
+    }
+}
