480923638146ca5065c11703b4224ff7eea64c93
Mike <33880049+Hiercy@users.noreply.github.com>
2018-12-21 17:04:48 +0300

    Espresso test

---
+++ RecyclerView/app/src/androidTest/java/com/example/volk1/recyclerview/RecyclerTest.java
@@ -0,0 +1,14 @@
+package com.example.volk1.recyclerview;
+
+import android.support.test.filters.LargeTest;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.runner.RunWith;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class RecyclerTest {
+
+    
+
+}
