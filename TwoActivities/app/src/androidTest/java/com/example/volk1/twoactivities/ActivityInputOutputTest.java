09fd9af7bcfeace8d15f7c2f73b29ef0ae8e5964
Mike <33880049+Hiercy@users.noreply.github.com>
2018-12-21 16:30:45 +0300

    Espresso test

---
+++ TwoActivities/app/src/androidTest/java/com/example/volk1/twoactivities/ActivityInputOutputTest.java
@@ -0,0 +1,43 @@
+package com.example.volk1.twoactivities;
+
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4.class)
+public class ActivityInputOutputTest {
+    @Test
+    public void useAppContext() {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getTargetContext();
+
+        assertEquals("com.example.volk1.twoactivities", appContext.getPackageName());
+    }
+
+    @Rule
+    public ActivityTestRule mActivityRule = new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void activityLaunch() {
+        onView(withId(R.id.button_main)).perform(click());
+        onView(withId(R.id.text_header)).check(matches(isDisplayed()));
+        onView(withId(R.id.b))
+    }
+}
