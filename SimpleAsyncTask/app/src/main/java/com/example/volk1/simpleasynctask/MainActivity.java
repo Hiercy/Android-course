7b75f55ff01b4ebd5a620e6bd4942d2bf8fb5273
Mike <33880049+Hiercy@users.noreply.github.com>
2018-12-21 23:35:40 +0300

    Async task

--- SimpleAsyncTask/app/src/main/java/com/example/volk1/simpleasynctask/MainActivity.java
+++ SimpleAsyncTask/app/src/main/java/com/example/volk1/simpleasynctask/MainActivity.java
@@ -2,12 +2,35 @@ package com.example.volk1.simpleasynctask;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TEXT_STATE = "currentState";
+
+    private TextView textView = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.textView1);
+
+        if (savedInstanceState != null) {
+            textView.setText(savedInstanceState.getString(TEXT_STATE));
+        }
+    }
+
+    public void startTask(View view) {
+        textView.setText("Napping...");
+        new SimpleAsyncTask(textView).execute();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        outState.putString(TEXT_STATE, textView.getText().toString());
     }
 }
