4b0d53914fa0079a161583787d3f13ef82a29d11
Mike <33880049+Hiercy@users.noreply.github.com>
2018-12-22 15:33:20 +0300

    Progress Bar

--- SimpleAsyncTask/app/src/main/java/com/example/volk1/simpleasynctask/MainActivity.java
+++ SimpleAsyncTask/app/src/main/java/com/example/volk1/simpleasynctask/MainActivity.java
@@ -3,12 +3,16 @@ package com.example.volk1.simpleasynctask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TEXT_STATE = "currentState";
 
     private TextView textView = null;
+    private TextView progressBar;
+
+    private ProgressBar progressBar2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,15 +20,18 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         textView = findViewById(R.id.textView1);
+        progressBar = findViewById(R.id.progress_bar);
+        progressBar2 = findViewById(R.id.progressBar22);
 
         if (savedInstanceState != null) {
             textView.setText(savedInstanceState.getString(TEXT_STATE));
+            progressBar.setText(savedInstanceState.getString(TEXT_STATE));
         }
     }
 
     public void startTask(View view) {
         textView.setText("Napping...");
-        new SimpleAsyncTask(textView).execute();
+        new SimpleAsyncTask(textView, progressBar, progressBar2).execute();
     }
 
     @Override
@@ -32,5 +39,6 @@ public class MainActivity extends AppCompatActivity {
         super.onSaveInstanceState(outState);
 
         outState.putString(TEXT_STATE, textView.getText().toString());
+        outState.putString(TEXT_STATE, progressBar.getText().toString());
     }
 }
--- SimpleAsyncTask/app/src/main/java/com/example/volk1/simpleasynctask/SimpleAsyncTask.java
+++ SimpleAsyncTask/app/src/main/java/com/example/volk1/simpleasynctask/SimpleAsyncTask.java
@@ -1,17 +1,24 @@
 package com.example.volk1.simpleasynctask;
 
 import android.os.AsyncTask;
+import android.view.View;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import java.lang.ref.WeakReference;
 import java.util.Random;
 
-public class SimpleAsyncTask extends AsyncTask<Void, Void, String> {
+public class SimpleAsyncTask extends AsyncTask<Void, Integer, String> {
 
     private WeakReference<TextView> mTextView;
+    private WeakReference<TextView> mProgressBar;
 
-    SimpleAsyncTask(TextView textView) {
+    private WeakReference<ProgressBar> mProgressBar2;
+
+    SimpleAsyncTask(TextView textView, TextView progress, ProgressBar progressBar2) {
         mTextView = new WeakReference<>(textView);
+        mProgressBar = new WeakReference<>(progress);
+        mProgressBar2 = new WeakReference<>(progressBar2);
     }
 
     @Override
@@ -19,10 +26,48 @@ public class SimpleAsyncTask extends AsyncTask<Void, Void, String> {
         Random r = new Random();
         int n = r.nextInt(11);
         int s = n * 200;
+        int i = 0;
+//        int j = 100;
 
         try {
-            Thread.sleep(s);
-        } catch(InterruptedException e) {
+            if (s == 0) {
+                publishProgress(100);
+                return "Awake at last after sleeping for " + s + " milliseconds!";
+            }
+//            while (i <= j) {
+//                publishProgress(i);
+//                i++;
+//                Thread.sleep(1);
+//            }
+            if (i != s / 4) {
+                for (i = 0; i < 25; i++) {
+                    publishProgress(i);
+                    i++;
+                    Thread.sleep(1);
+                }
+            }
+            if (i != s / 3) {
+                for (i = 25; i < 33; i++) {
+                    publishProgress(i);
+                    i++;
+                    Thread.sleep(1);
+                }
+            }
+            if (i != s / 2) {
+                for (i = 33; i < 50; i++) {
+                    publishProgress(i);
+                    i++;
+                    Thread.sleep(1);
+                }
+            }
+            if (i != s) {
+                for (i = 50; i <= 100; i++) {
+                    publishProgress(i);
+                    i++;
+                    Thread.sleep(1);
+                }
+            }
+        } catch (InterruptedException e) {
             e.printStackTrace();
         }
 
@@ -33,4 +78,13 @@ public class SimpleAsyncTask extends AsyncTask<Void, Void, String> {
     protected void onPostExecute(String result) {
         mTextView.get().setText(result);
     }
+
+    @Override
+    protected void onProgressUpdate(Integer... progress) {
+        super.onProgressUpdate(progress);
+        mProgressBar.get().setText("progress: " + (progress[0]) + "%");
+        mProgressBar2.get().setVisibility(View.VISIBLE);
+        mProgressBar2.get().setProgress(progress[0]);
+    }
+
 }
