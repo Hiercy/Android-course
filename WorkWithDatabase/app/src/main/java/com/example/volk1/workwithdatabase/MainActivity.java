0823ba9d6be221e8ddae62d464791b57194a3df1
Mike <33880049+Hiercy@users.noreply.github.com>
2019-01-15 23:50:31 +0300

    Update UI

--- WorkWithDatabase/app/src/main/java/com/example/volk1/workwithdatabase/MainActivity.java
+++ WorkWithDatabase/app/src/main/java/com/example/volk1/workwithdatabase/MainActivity.java
@@ -1,16 +1,14 @@
 package com.example.volk1.workwithdatabase;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
-import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 import java.util.ArrayList;
 
@@ -48,8 +46,9 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent intent = new Intent(MainActivity.this, NewQuestionActivity.class);
+                // TODO: change to startActivityForResult(intent, int);
+                startActivity(intent);
             }
         });
 
@@ -76,9 +75,6 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
-    }
-
-    public void newQuestion(View view) {
     }
 
     private void initData() {
---
+++ WorkWithDatabase/app/src/main/java/com/example/volk1/workwithdatabase/NewQuestionActivity.java
@@ -0,0 +1,17 @@
+package com.example.volk1.workwithdatabase;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class NewQuestionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_question);
+    }
+
+    public void addNewQuestion(View view) {
+    }
+}
