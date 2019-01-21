b04a00d348a209102c38d93f4d9914305e10cf26
Mike <33880049+Hiercy@users.noreply.github.com>
2019-01-21 22:50:08 +0300

    Parse and json processing

--- WheatherApp/app/src/main/java/com/example/volk1/wheatherapp/MainActivity.java
+++ WheatherApp/app/src/main/java/com/example/volk1/wheatherapp/MainActivity.java
@@ -1,7 +1,6 @@
 package com.example.volk1.wheatherapp;
 
 import android.Manifest;
-import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
@@ -10,7 +9,6 @@ import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.design.widget.Snackbar;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -18,22 +16,29 @@ import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.loopj.android.http.AsyncHttpClient;
 import com.loopj.android.http.JsonHttpResponseHandler;
 import com.loopj.android.http.RequestParams;
 
+import org.json.JSONArray;
 import org.json.JSONObject;
-
-import java.net.URL;
 
 import cz.msebera.android.httpclient.Header;
 
 public class MainActivity extends AppCompatActivity {
+private static final String TAG = "Weather";
 
-    private static final String OPEN_WEATHER = "api.openweathermap.org/data/2.5/weather?q=%s&units=metric";
+
+    private final int REQUEST = 123;
+
+    private static final String OPEN_WEATHER = "http://api.openweathermap.org/data/2.5/weather";
     private static final String APP_ID = "d99db4c4b8343fa2a97ca15896a38186";
+
+    // Time between location updates
+    long MIN_TIME = 5000;
+    // Distance between location updates
+    float MIN_DISTANCE = 1000;
 
     String LOCATION_PROVIDER = LocationManager.GPS_PROVIDER;
 
@@ -77,24 +82,6 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-//    public void getJSON(RequestParams params) {
-//        AsyncHttpClient asyncHttpClient = new AsyncHttpClient();
-//
-//        asyncHttpClient.get(OPEN_WEATHER, params, new JsonHttpResponseHandler() {
-//            @Override
-//            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
-//
-//
-//            }
-//
-//            @Override
-//            public void onFailure(int statusCode, Header[] headers, Throwable e, JSONObject response) {
-//
-//
-//            }
-//        });
-//    }
-
     @Override
     public void onResume() {
         super.onResume();
@@ -103,12 +90,25 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void getWeatherForCurrentLocation() {
+        Log.d("Weather", "getWeatherForCurrentLocation() called");
         mLocationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
 
         mLocationListener = new LocationListener() {
             @Override
             public void onLocationChanged(Location location) {
+                Log.d(TAG, "onLocationChanged() called");
 
+                String longitude = String.valueOf(location.getLongitude());
+                String latitude = String.valueOf(location.getLatitude());
+
+                Log.d(TAG, "longitude is: " + longitude);
+                Log.d(TAG, "latitude is: " + latitude);
+
+                RequestParams requestParams = new RequestParams();
+                requestParams.put("lon", longitude);
+                requestParams.put("lat", latitude);
+                requestParams.put("appid", APP_ID);
+                letsDoSomeNetworking(requestParams);
             }
 
             @Override
@@ -126,5 +126,55 @@ public class MainActivity extends AppCompatActivity {
 
             }
         };
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            ActivityCompat.requestPermissions(
+                    this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    REQUEST);
+
+            return;
+        }
+        mLocationManager.requestLocationUpdates(LOCATION_PROVIDER, MIN_TIME, MIN_DISTANCE, mLocationListener);
+    }
+
+    private void letsDoSomeNetworking(final RequestParams requestParams) {
+        AsyncHttpClient asyncHttpClient = new AsyncHttpClient();
+
+        asyncHttpClient.get(OPEN_WEATHER, requestParams, new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                Log.d(TAG, "Success! JSON: " + response);
+
+                Weather weather = Weather.fromJSON(response);
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONArray errorResponse) {
+                Log.e(TAG, "Fail " + throwable.toString());
+                Log.d(TAG, "Status code " + statusCode);
+            }
+        });
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == REQUEST) {
+            // If user access permission
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                getWeatherForCurrentLocation();
+            } else {
+                Log.d(TAG, "Permission denied");
+            }
+        }
     }
 }
--- WheatherApp/app/src/main/java/com/example/volk1/wheatherapp/SelectCityActivity.java
+++ WheatherApp/app/src/main/java/com/example/volk1/wheatherapp/SelectCityActivity.java
@@ -2,8 +2,8 @@ package com.example.volk1.wheatherapp;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.inputmethod.EditorInfo;
