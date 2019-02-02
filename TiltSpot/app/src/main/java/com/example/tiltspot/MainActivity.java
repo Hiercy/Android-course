025a8a5140c10dd48c162d362619797cbe0a684a
Mike <33880049+Hiercy@users.noreply.github.com>
2019-02-02 12:51:24 +0300

    Calculate and Update UI in AsyncTask

--- TiltSpot/app/src/main/java/com/example/tiltspot/MainActivity.java
+++ TiltSpot/app/src/main/java/com/example/tiltspot/MainActivity.java
@@ -6,6 +6,7 @@ import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Display;
@@ -111,105 +112,7 @@ public class MainActivity extends AppCompatActivity
 
     @Override
     public void onSensorChanged(SensorEvent sensorEvent) {
-        // The sensor type (as defined in the Sensor class).
-        int sensorType = sensorEvent.sensor.getType();
-
-        // The sensorEvent object is reused across calls to onSensorChanged().
-        // clone() gets a copy so the data doesn't change out from under us
-        switch (sensorType) {
-            case Sensor.TYPE_ACCELEROMETER:
-                mAccelerometerData = sensorEvent.values.clone();
-                break;
-            case Sensor.TYPE_MAGNETIC_FIELD:
-                mMagnetometerData = sensorEvent.values.clone();
-                break;
-            default:
-                return;
-        }
-        // Compute the rotation matrix: merges and translates the data
-        // from the accelerometer and magnetometer, in the device coordinate
-        // system, into a matrix in the world's coordinate system.
-        //
-        // The second argument is an inclination matrix, which isn't
-        // used in this example.
-        float[] rotationMatrix = new float[9];
-        boolean rotationOK = SensorManager.getRotationMatrix(rotationMatrix,
-                null, mAccelerometerData, mMagnetometerData);
-
-        // Remap the matrix based on current device/activity rotation.
-        float[] rotationMatrixAdjusted = new float[9];
-        switch (mDisplay.getRotation()) {
-            case Surface.ROTATION_0:
-                rotationMatrixAdjusted = rotationMatrix.clone();
-                break;
-            case Surface.ROTATION_90:
-                SensorManager.remapCoordinateSystem(rotationMatrix,
-                        SensorManager.AXIS_Y, SensorManager.AXIS_MINUS_X,
-                        rotationMatrixAdjusted);
-                break;
-            case Surface.ROTATION_180:
-                SensorManager.remapCoordinateSystem(rotationMatrix,
-                        SensorManager.AXIS_MINUS_X, SensorManager.AXIS_MINUS_Y,
-                        rotationMatrixAdjusted);
-                break;
-            case Surface.ROTATION_270:
-                SensorManager.remapCoordinateSystem(rotationMatrix,
-                        SensorManager.AXIS_MINUS_Y, SensorManager.AXIS_X,
-                        rotationMatrixAdjusted);
-                break;
-        }
-
-        // Get the orientation of the device (azimuth, pitch, roll) based
-        // on the rotation matrix. Output units are radians.
-        float orientationValues[] = new float[3];
-        if (rotationOK) {
-            SensorManager.getOrientation(rotationMatrixAdjusted,
-                    orientationValues);
-        }
-
-        // Pull out the individual values from the array.
-        float azimuth = orientationValues[0];
-        float pitch = orientationValues[1];
-        float roll = orientationValues[2];
-
-        // Pitch and roll values that are close to but not 0 cause the
-        // animation to flash a lot. Adjust pitch and roll to 0 for very
-        // small values (as defined by VALUE_DRIFT).
-        if (Math.abs(pitch) < VALUE_DRIFT) {
-            pitch = 0;
-        }
-        if (Math.abs(roll) < VALUE_DRIFT) {
-            roll = 0;
-        }
-
-        // Fill in the string placeholders and set the textview text.
-        mTextSensorAzimuth.setText(getResources().getString(
-                R.string.value_format, azimuth));
-        mTextSensorPitch.setText(getResources().getString(
-                R.string.value_format, pitch));
-        mTextSensorRoll.setText(getResources().getString(
-                R.string.value_format, roll));
-
-        // Reset all spot values to 0. Without this animation artifacts can
-        // happen with fast tilts.
-        mSpotTop.setAlpha(0f);
-        mSpotBottom.setAlpha(0f);
-        mSpotLeft.setAlpha(0f);
-        mSpotRight.setAlpha(0f);
-
-        // Set spot color (alpha/opacity) equal to pitch/roll.
-        // this is not a precise grade (pitch/roll can be greater than 1)
-        // but it's close enough for the animation effect.
-        if (pitch > 0) {
-            mSpotBottom.setAlpha(pitch);
-        } else {
-            mSpotTop.setAlpha(Math.abs(pitch));
-        }
-        if (roll > 0) {
-            mSpotLeft.setAlpha(roll);
-        } else {
-            mSpotRight.setAlpha(Math.abs(roll));
-        }
+        new CalcAsyncTask(sensorEvent).execute();
     }
 
     /**
@@ -218,5 +121,110 @@ public class MainActivity extends AppCompatActivity
      */
     @Override
     public void onAccuracyChanged(Sensor sensor, int i) {
+    }
+
+    class CalcAsyncTask extends AsyncTask<Void, Void, float[]> {
+
+        SensorEvent sensorEvent;
+
+        CalcAsyncTask(SensorEvent sensorEvent) {
+            this.sensorEvent = sensorEvent;
+        }
+
+        @Override
+        protected float[] doInBackground(Void... voids) {
+            int sensorType = sensorEvent.sensor.getType();
+
+            // The sensorEvent object is reused across calls to onSensorChanged().
+            // clone() gets a copy so the data doesn't change out from under us
+            switch (sensorType) {
+                case Sensor.TYPE_ACCELEROMETER:
+                    mAccelerometerData = sensorEvent.values.clone();
+                    break;
+                case Sensor.TYPE_MAGNETIC_FIELD:
+                    mMagnetometerData = sensorEvent.values.clone();
+                    break;
+                default:
+                    return null;
+            }
+            // Compute the rotation matrix: merges and translates the data
+            // from the accelerometer and magnetometer, in the device coordinate
+            // system, into a matrix in the world's coordinate system.
+            //
+            // The second argument is an inclination matrix, which isn't
+            // used in this example.
+            float[] rotationMatrix = new float[9];
+            boolean rotationOK = SensorManager.getRotationMatrix(rotationMatrix,
+                    null, mAccelerometerData, mMagnetometerData);
+
+            // Remap the matrix based on current device/activity rotation.
+            float[] rotationMatrixAdjusted = new float[9];
+            switch (mDisplay.getRotation()) {
+                case Surface.ROTATION_0:
+                    rotationMatrixAdjusted = rotationMatrix.clone();
+                    break;
+                case Surface.ROTATION_90:
+                    SensorManager.remapCoordinateSystem(rotationMatrix,
+                            SensorManager.AXIS_Y, SensorManager.AXIS_MINUS_X,
+                            rotationMatrixAdjusted);
+                    break;
+                case Surface.ROTATION_180:
+                    SensorManager.remapCoordinateSystem(rotationMatrix,
+                            SensorManager.AXIS_MINUS_X, SensorManager.AXIS_MINUS_Y,
+                            rotationMatrixAdjusted);
+                    break;
+                case Surface.ROTATION_270:
+                    SensorManager.remapCoordinateSystem(rotationMatrix,
+                            SensorManager.AXIS_MINUS_Y, SensorManager.AXIS_X,
+                            rotationMatrixAdjusted);
+                    break;
+            }
+
+            // Get the orientation of the device (azimuth, pitch, roll) based
+            // on the rotation matrix. Output units are radians.
+            float orientationValues[] = new float[3];
+            if (rotationOK) {
+                SensorManager.getOrientation(rotationMatrixAdjusted,
+                        orientationValues);
+            }
+
+            return orientationValues;
+        }
+
+        @Override
+        protected void onPostExecute(float[] floats) {
+            float azimuth = floats[0];
+            float pitch = floats[1];
+            float roll = floats[2];
+
+            // Fill in the string placeholders and set the textview text.
+            mTextSensorAzimuth.setText(getResources().getString(
+                    R.string.value_format, azimuth));
+            mTextSensorPitch.setText(getResources().getString(
+                    R.string.value_format, pitch));
+            mTextSensorRoll.setText(getResources().getString(
+                    R.string.value_format, roll));
+
+            // Reset all spot values to 0. Without this animation artifacts can
+            // happen with fast tilts.
+            mSpotTop.setAlpha(0f);
+            mSpotBottom.setAlpha(0f);
+            mSpotLeft.setAlpha(0f);
+            mSpotRight.setAlpha(0f);
+
+            // Set spot color (alpha/opacity) equal to pitch/roll.
+            // this is not a precise grade (pitch/roll can be greater than 1)
+            // but it's close enough for the animation effect.
+            if (pitch > 0) {
+                mSpotBottom.setAlpha(pitch);
+            } else {
+                mSpotTop.setAlpha(Math.abs(pitch));
+            }
+            if (roll > 0) {
+                mSpotLeft.setAlpha(roll);
+            } else {
+                mSpotRight.setAlpha(Math.abs(roll));
+            }
+        }
     }
 }
